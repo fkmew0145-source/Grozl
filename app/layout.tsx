959261dcf8ca0ff4bc4eacb3bc827027deb516c9
Grozl <fkmew0145@gmail.com>
2026-04-01 20:51:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,12 +38,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="font-sans antialiased">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
           {children}
           <Analytics />
         </ThemeProvider>
       </body>
     </html>
   )
-        }
-        
+}
